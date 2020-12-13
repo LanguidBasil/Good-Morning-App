4220a31bdb239c547f251a515c7e7796300a6336
LanguidBasil <e.makunev@gmail.com>
2020-12-13 19:48:36 +1000

    Code looks a bit cleaner now

--- good_morning_app.py
+++ good_morning_app.py
@@ -9,14 +9,6 @@ from tkinter import filedialog as fd
 
 def Main ():
 
-    last_used_path = ""
-    if (os.path.exists("Good_Morning_App_settings")):
-        with open("Good_Morning_App_settings", "r") as f:
-            last_used_path = f.read()
-    else:
-        print("Can't find settings")
-
-
     def focus (event):
         event.widget.focus()
         
@@ -35,7 +27,7 @@ def Main ():
             if site:
                 site_frame = tk.Frame(main_frame, bg = "#ffffff")
                 site_frames.append(site_frame)
-                site = tk.Radiobutton(site_frame, text=f"{sites[index]}", height = 2, font = ("Times new roman", 10), bg = "#ffffff", indicatoron = 0, bd = 0, variable = active_var, value = index, command = site_focus)
+                site = tk.Radiobutton(site_frame, text=f"{sites[index]}", height = 2, font = ("Times new roman", 10), bg = "#ffffff", indicatoron = 0, bd = 0, variable = focused_site_var, value = index, command = site_focus)
                 site_radiobuttons.append(site)
                 site.pack(side = tk.LEFT)
                 site_frame.grid(row = index, column = 0, sticky = "nswe")
@@ -50,8 +42,10 @@ def Main ():
         site_radiobuttons.clear()
         site_frames.clear()
         sites_update()
+        
+    # ---------------------------------------------------- Buttons functions ------------------------------------------------------
 
-    def add():
+    def button_add():
         new_site = str(adder_entry.get())
         is_valid = validators.url(new_site)
         if (is_valid):
@@ -62,9 +56,9 @@ def Main ():
             tk.messagebox.showwarning(title="Warning", message="This URL is not valid")
         root.focus()
 
-    def delete():
+    def button_delete():
         if root.focus_get() and sites:
-            del sites[active_var.get()]
+            del sites[focused_site_var.get()]
 
             for widget in main_frame.winfo_children():
                 widget.destroy()
@@ -74,17 +68,17 @@ def Main ():
             sites_update()
         root.focus()
 
-    def delete_all():
+    def button_delete_all():
         if sites:
             clear_sites()
         root.focus()
 
-    def run():
+    def button_run():
         for site in sites:
             webbrowser.open_new_tab(site)
         root.focus()
 
-    def save():
+    def button_save():
         file_name = fd.asksaveasfilename(
                 defaultextension='.json', filetypes=[("json files", '*.json')],
                 title="Choose filename")
@@ -99,12 +93,11 @@ def Main ():
                         ensure_ascii=False)
 
             last_used_path = file_name
-            with open("Good_Morning_App_settings", "w") as _f:
+            with open(SETTINGSFILENAME, "w") as _f:
                 _f.write(last_used_path)
         root.focus()
 
-    def my_open(*args):
-
+    def button_open(*args):
 
         if args:
             clear_sites()
@@ -123,11 +116,13 @@ def Main ():
                         #print(site)
                         sites.append(site)
                 last_used_path = file_name
-                with open("settings", "w") as _f:
+                with open(SETTINGSFILENAME, "w") as _f:
                     _f.write(last_used_path)
 
         root.focus()
         sites_update()
+        
+    # ---------------------------------------------------- Creating layout ------------------------------------------------------
 
     root = tk.Tk()
     root.title("Good morning app")
@@ -155,34 +150,44 @@ def Main ():
     adder_entry.grid(row = 0, column = 2, columnspan = 2, padx = (10, 10), pady = (5, 0), sticky = "w")
     adder_entry.focus_set()
 
-    add_button = tk.Button(upper_frame, text = "Add", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = add)
+    add_button = tk.Button(upper_frame, text = "Add", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = button_add)
     add_button.grid(row = 0, column = 4, pady = (5, 0), sticky = "w")
 
-    delete_button = tk.Button(upper_frame, text = "Delete", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = delete)
+    delete_button = tk.Button(upper_frame, text = "Delete", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = button_delete)
     delete_button.grid(row = 0, column = 0, pady = (5, 0), sticky = "e")
 
-    delete_all_button = tk.Button(upper_frame, text = "Delete All", font = ("Times new roman", 10), width = 8, bg = "#d8d9d9", fg = "#e57340", command = delete_all)
+    delete_all_button = tk.Button(upper_frame, text = "Delete All", font = ("Times new roman", 10), width = 8, bg = "#d8d9d9", fg = "#e57340", command = button_delete_all)
     delete_all_button.grid(row = 0, column = 1, pady = (5, 0), sticky = "e")
 
-    run_button = tk.Button(bottom_frame, text = "Run", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = run)
+    run_button = tk.Button(bottom_frame, text = "Run", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = button_run)
     run_button.grid(row = 0, column = 4, pady = 6, sticky = "w")
     
-    save_button = tk.Button(bottom_frame, text = "Save", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = save)
+    save_button = tk.Button(bottom_frame, text = "Save", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = button_save)
     save_button.grid(row = 0, column = 1, pady = 6, sticky = "e")
     
-    open_button = tk.Button(bottom_frame, text = "Open", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = my_open)
+    open_button = tk.Button(bottom_frame, text = "Open", font = ("Times new roman", 10), width = 6, bg = "#d8d9d9", fg = "#e57340", command = button_open)
     open_button.grid(row = 0, column = 0, pady = 6, sticky = "e")
-    
 
-    active_var = tk.IntVar()
-    active_var.set(0)
+    # ---------------------------------------------------- Prepairing to launch ------------------------------------------------------
+    
+    SETTINGSFILENAME = "Good_Morning_App_settings"
+    last_used_path = ""
+
+    focused_site_var = tk.IntVar()
+    focused_site_var.set(0)
 
     sites = []
     site_radiobuttons = []
     site_frames = []
-    sites_update()
+
+    if (os.path.exists("Good_Morning_App_settings")):
+        with open(SETTINGSFILENAME, "r") as f:
+            last_used_path = f.read()
+    else:
+        print("Can't find settings")
+
     if last_used_path:
-        my_open(last_used_path)
+        button_open(last_used_path)
 
     upper_frame.grid(row = 0, column = 0, columnspan = 5, sticky = "nswe")
     main_frame.grid(row = 1, column = 0, columnspan = 5, sticky = "nswe")
